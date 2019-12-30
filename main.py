ba8a046359fbdb6bfc71a9e5359bc333db48852e
Th. Chousiadas <th.chousiadas@gmail.com>
2019-12-30 23:02:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
 import serial
-import json
 import RPi.GPIO as GPIO
+import logging
+
+# initialize the log settings
+logging.basciConfig(filename='app.log', format='%(process)d - %(levelname)s: %(asctime)s - %(message)s', level=logging.INFO)
 
 # Configure the communication with HC-12 board using USART protocol
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
@@ -26,52 +28,35 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Home Sensors").sheet1
 
-with open("info_file.json", "r+") as f:
-    data = json.load(f)
-    if(data["count"] == 1):
-        labels = ['Date', 'Time', 'Humidity', 'Temperature', 'Moisture']
-        # FIRST ROW
-        sheet.insert_row(labels, 1)
-        #print("A new sheet of measurements has begun")
-        indexRow = 2
-        data["count"] = 2
-        f.seek(0)
-        json.dump(data, f, indent=4)
-        f.truncate()
-    else:
-        indexRow = data["count"]
-        
+# check the first row. If labels not exist then append the labels
+if (len(sheet.row_values(1))== 0):
+    sheet.append_row(labels)      
         
 def main(scope, sheet):
-
     while(1):
        if creds.access_token_expired:
+           logging.info('Access token expired.')
            client.login()
        try:
            data = ser.readline()
            if(data):
-               # Take the current time
-               curr_time = str(datetime.datetime.now().time())
                # Split the data
                data = data.decode().split('#')
+               # Take the current time
+               curr_time = str(datetime.datetime.now().time())
                # Insert in the first element of the list the time
                data.insert(0, curr_time)
+               # same for date
                curr_day = str(datetime.date.today())
                data.insert(0, curr_day)
-               # write the row in the Google Sheets
-               with open("info_file.json", "r+") as f:
-                   idx = json.load(f)
-                   sheet.insert_row(data, idx["count"])
-                   idx["count"] += 1
-                   f.seek(0)
-                   json.dump(idx, f, indent=4)
-                   f.truncate()
-                   #print("The json file has been modified")
+               # append row
+               sheet.append_row(data)
 
-       except Exception:
-           traceback.print_exc()
+       except KeyboardInterrupt:
+           logging.info("Keyboard Interrupt")
+           break
 
-
+         
 if __name__ == "__main__":
     main(scope, sheet)
 
